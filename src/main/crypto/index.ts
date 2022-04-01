7cc2fc8035412450901f20d5431c075b9aa3c174
emil <poolsar.42@gmail.com>
2022-04-01 16:35:39 +0800

    encryption public private

--- src/main/crypto/index.ts
+++ src/main/crypto/index.ts
@@ -1,10 +1,17 @@
 import { encrypt, decrypt, PrivateKey } from 'eciesjs';
 
-export const privateKey = new PrivateKey();
+export const privateKey = () => {
+  return PrivateKey.fromHex(
+    '0x2a1d5d208b3d551e8662bcf4bd12e66ab4e025ec8ef6e18cbc40c1594794652f'
+  );
+};
 
 export const encryptMsg = (message: string, privKey: PrivateKey) => {
   const data = Buffer.from(message);
-  return encrypt(privateKey.publicKey.toHex(), data);
+  return encrypt(
+    '024dc4941523c2a0af57528d18a86c35ad24d3e5966788e1807ffd83391a5f6668',
+    data
+  );
 };
 
 export const decryptMsg = (cipher: any, privKey: PrivateKey) => {
--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,6 +3,12 @@ const store = require('../renderer/state');
 const { sendMessage } = require('../renderer/state/actionCreators/chat');
 const { privateKey, decryptMsg, encryptMsg } = require('./crypto');
 
+/** Private Key:
+/*  0x2a1d5d208b3d551e8662bcf4bd12e66ab4e025ec8ef6e18cbc40c1594794652f
+/*  Public Key:
+/*  024dc4941523c2a0af57528d18a86c35ad24d3e5966788e1807ffd83391a5f6668
+*/
+
 contextBridge.exposeInMainWorld('electron', {
   ipc: { ...ipcRenderer, on: ipcRenderer.on, once: ipcRenderer.once },
   store,
