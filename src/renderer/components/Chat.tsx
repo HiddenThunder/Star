4513d76ed018d364d1d3669c5082228071b09389
emil <poolsar.42@gmail.com>
2022-04-10 13:40:16 +0800

    one chat => multiple channels

--- src/renderer/components/Chat.tsx
+++ src/renderer/components/Chat.tsx
@@ -1,22 +1,21 @@
 import { useSelector } from 'react-redux';
 
 const Chat = () => {
-  const messages = useSelector((state) => state.chat);
   const channel = useSelector((state) => state.channel);
+  const topic = channel.topic ? channel.topic : 'lobby';
+  const messages = useSelector((state) => state.chat[topic]);
 
   return (
     <div id="chat" className="inline">
       <h3 className="header">Messages</h3>
       <ul className="no-bullets">
-        {messages
-          .filter((message: any) => message.channel == channel.topic)
-          .map((message: any, index: any) => {
-            return (
-              <li className="item" key={index}>
-                {message.content}
-              </li>
-            );
-          })}
+        {messages.map((message: any, index: any) => {
+          return (
+            <li className="item" key={index}>
+              {message.content}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/renderer/state/redux/chat.ts
+++ src/renderer/state/redux/chat.ts
@@ -4,12 +4,25 @@ const SEND_MESSAGE = 'SEND_MESSAGE';
 const DELETE_MESSAGE = 'DELETE_MESSAGE';
 
 // REDUCER
-export default function chatReducer(chat: any = [], action: any) {
+export default function chatReducer(chat: any = { lobby: [] }, action: any) {
   switch (action.type) {
     case SEND_MESSAGE:
-      return [...chat, action.message];
+      return chat[`${action.message.channel}`]
+        ? {
+            ...chat,
+            [action.message.channel]: [
+              ...chat[`${action.message.channel}`],
+              action.message,
+            ],
+          }
+        : { ...chat, [action.message.channel]: [action.message] };
     case DELETE_MESSAGE:
-      return chat.filter((msg: any) => msg.id !== action.messageId);
+      return {
+        ...chat,
+        [action.message.channel]: chat[`${action.message.channel}`].filter(
+          (msg: any) => msg.id !== action.messageId
+        ),
+      };
     default:
       return chat;
   }
